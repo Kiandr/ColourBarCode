399d9543498e04cd1adb949568654ac06f3652cc
kiandr <davoudi.rk@gmail.com>
2016-05-20 13:54:58 -0700

    Inital commit ground zero!

---
+++ ColourBarCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColourBarCode
+//
+//  Created by Kian Davoudi-Rad on 2016-05-20.
+//  Copyright © 2016 Kian Davoudi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ColourBarCode/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ColourBarCode
+//
+//  Created by Kian Davoudi-Rad on 2016-05-20.
+//  Copyright © 2016 Kian Davoudi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
